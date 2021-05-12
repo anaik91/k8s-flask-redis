e4cb7a8c7ff995cf9317882335c26482bcf46394
ASHWIN KUMAR NAIK <ashwinkumarnaik91@gmail.com>
2021-05-13 00:00:38 +0530

    inital

---
+++ src/app.py
@@ -0,0 +1,23 @@
+import time
+import os
+import redis
+from flask import Flask
+
+app = Flask(__name__)
+cache = redis.Redis(host=os.getenv('REDIS_URL','localhost'), port=int(os.getenv('REDIS_PORT',6379)))
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
